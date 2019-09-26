d75cb157684a73857dade99a4d0c09da3f41653c
helloitsjoe <jboyle617@gmail.com>
2019-09-25 23:01:06 -0400

    Comment out week 3

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import MatildasCupcakes from './week-01/App';
 import MarthaDentist from './week-02/App';
-import SweatySeth from './week-03/App';
+// import SweatySeth from './week-03/App';
 
 import './index.css';
 
-const weeks = [MatildasCupcakes, MarthaDentist, SweatySeth];
+const weeks = [MatildasCupcakes, MarthaDentist /* , SweatySeth */];
 
 // This assumes that the last query param will be the initialIndex
-const q = window ? window.location.search : '';
-const initialIndex = q.substring(q.lastIndexOf('=') + 1) || weeks[weeks.length - 1];
+// const q = window ? window.location.search : '';
+// const initialIndex = q.substring(q.lastIndexOf('=') + 1) || weeks[weeks.length - 1];
 
 function App() {
-  const [currentWeek, setCurrentWeek] = useState(initialIndex);
+  const [currentWeek, setCurrentWeek] = useState(1);
   const Main = weeks[currentWeek];
   return (
     <div>
@@ -26,7 +26,7 @@ function App() {
       >
         <option value={0}>Week 1: Matilda&apos;s Cupcakes</option>
         <option value={1}>Week 2: Martha&apos;s Dentapalooza</option>
-        <option value={2}>Week 3: Sweatin&apos; with Seth</option>
+        {/* <option value={2}>Week 3: Sweatin&apos; with Seth</option> */}
       </select>
     </div>
   );
